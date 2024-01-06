6f0e19011d6db095972884db327f053813b9164a
Sir Abdulkarim Hussain SITD <abdulkarimhussain7@gmail.com>
2024-01-06 19:47:20 +0100

    menu in virtual_class

--- students/virtual_class.php
+++ students/virtual_class.php
@@ -486,68 +486,6 @@ if (isset($_GET['id'])) {
     <!-- partial -->
     <div class="container-fluid page-body-wrapper">
       <!-- partial:partials/_sidebar.html -->
-      <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-          <li class="nav-item nav-profile">
-            <a href="#" class="nav-link">
-              <div class="profile-image">
-                <img class="img-xs rounded-circle" src="../assets/images/faces/face8.jpg" alt="profile image">
-                <div class="dot-indicator bg-success"></div>
-              </div>
-              <div class="text-wrapper">
-                <p class="profile-name">Student</p>
-                
-              </div>
-            </a>
-          </li>
-          <li class="nav-item nav-category">Main Menu</li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <i class="menu-icon typcn typcn-document-text"></i>
-              <span class="menu-title">Dashboard</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="subjects.php" aria-expanded="false">
-              <i class="menu-icon typcn typcn-coffee"></i>
-              <span class="menu-title">Subjects</span>
-              <i class="menu-arrow"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" aria-expanded="false">
-              <i class="menu-icon typcn typcn-coffee"></i>
-              <span class="menu-title">Timetable</span>
-              <i class="menu-arrow"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" aria-expanded="false">
-              <i class="menu-icon typcn typcn-coffee"></i>
-              <span class="menu-title">Assessments</span>
-              <i class="menu-arrow"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <i class="menu-icon typcn typcn-shopping-bag"></i>
-              <span class="menu-title">Calendar</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <i class="menu-icon typcn typcn-th-large-outline"></i>
-              <span class="menu-title">Events</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <i class="menu-icon typcn typcn-bell"></i>
-              <span class="menu-title">Notifications Center</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
       <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper" style="width: 100vw">
